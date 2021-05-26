6edaf1b58aab99d4319301c1eb751744d9fc744a
Elias Santana <eliasantanasilva@gmail.com>
2021-05-25 21:22:22 -0300

    Aula5 - Criando Configurations - @AutoWired, @Bean, @Configuration

---
+++ src/main/java/io/github/douglasfps/MinhaConfiguration.java
@@ -0,0 +1,16 @@
+package io.github.douglasfps;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class MinhaConfiguration {
+    @Bean (name="applicationName")
+    public String applicationName(){
+        return  "Sistema de Vendas";
+    }
+
+    @Bean(name="version")
+    public String version(){
+        return "1.0";
+    }
+}
--- src/main/java/io/github/douglasfps/VendasAplication.java
+++ src/main/java/io/github/douglasfps/VendasAplication.java
@@ -1,5 +1,7 @@
 package io.github.douglasfps;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class VendasAplication {
+    @Autowired
+    @Qualifier("applicationName")
+    private String aplicatioinName;
+
+    @Autowired
+    @Qualifier("version")
+    private String version;
+
     @GetMapping("/hello")
     public String helloWord(){
-        return "HelloWord";
+        return aplicatioinName + " - " + version;
     }
+
+
     public static void main(String[] args) {
         SpringApplication.run(VendasAplication.class, args);
     }
