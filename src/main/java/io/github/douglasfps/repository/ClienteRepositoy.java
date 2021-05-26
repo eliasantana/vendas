d002210b0df4ce24d0bc95718c6c691487a068aa
Elias Santana <eliasantanasilva@gmail.com>
2021-05-26 20:48:18 -0300

    Aula6 - Ingeção de Dependências

---
+++ src/main/java/io/github/douglasfps/repository/ClienteRepositoy.java
@@ -0,0 +1,11 @@
+package io.github.douglasfps.repository;
+
+import io.github.douglasfps.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClienteRepositoy {
+    public void persistir(Cliente cliente) {
+        //Acessa a base de dados e salva o cliente
+    }
+}
