05816ef348ce7db74dff37bcb9f146bc2161a6b4
Elias Santana <eliasantanasilva@gmail.com>
2021-05-26 20:15:05 -0300

    Aula 5 - Container IOC do Spirng

--- src/main/java/io/github/douglasfps/VendasAplication.java
+++ src/main/java/io/github/douglasfps/VendasAplication.java
@@ -4,11 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+/** Colocar uma classe para ser gerenciada pelo container do Spring
+@ComponentScan(basePackages = {
+                    "io.github.douglasfps.repository",
+                    "io.github.douglasfps.service",
+                    "br.com.outraconfiguracaobiblioteca.projeto"})
+
+ * Adicionamos na notação @ConpomentScan os pacotes que queremos que o SpringBoot Scanei
+ * */
 public class VendasAplication {
     @Autowired
     @Qualifier("applicationName")
---
+++ src/main/java/io/github/douglasfps/service/MeuServico.java
@@ -0,0 +1,4 @@
+package io.github.douglasfps.service;
+
+public class MeuServico {
+}
