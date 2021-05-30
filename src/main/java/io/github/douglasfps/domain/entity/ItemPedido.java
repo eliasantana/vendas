f89e85c480a245c19da8c555dc1416fbaefbff55
Elias Santana <eliasantanasilva@gmail.com>
2021-05-30 08:48:22 -0300

    aula11 - Criando as classes da entidade do banco e adicionando o pacote domain.entity

---
+++ src/main/java/io/github/douglasfps/domain/entity/ItemPedido.java
@@ -0,0 +1,51 @@
+package io.github.douglasfps.domain.entity;
+
+import java.math.BigDecimal;
+
+public class ItemPedido {
+    private Integer id;
+    private Pedido pedido;
+    private Produto produto;
+    private BigDecimal total;
+    private Integer qtd;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public Integer getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(Integer qtd) {
+        this.qtd = qtd;
+    }
+}
